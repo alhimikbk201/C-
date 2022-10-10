1794f75e6d0dd271b37589dbf5a85fac396aefdc
Alexandra788 <andeytrof45@gmail.com>
2022-10-11 00:00:13 +0700

    Решена задача 4 из первой лекции

--- basic/Program.cs
+++ basic/Program.cs
@@ -1,20 +1,36 @@
-﻿int a, b;
+﻿int a, b, c;
 
-string number1, number2;
+string number1, number2, number3;
 Console.Write("Введите первое число");
 
 number1 = Console.ReadLine()!;
 
 int.TryParse(number1, out a);
 
-Console.Write("Введите первое число");
+Console.Write("Введите второе число");
 
 number2 = Console.ReadLine()!;
 
 int.TryParse(number2, out b);
 
-if(a > b) {
-    Console.Write($"max {a}");
+Console.Write("Введите третье число");
+
+number3 = Console.ReadLine()!;
+
+int.TryParse(number3, out c);
+
+
+
+if(a > b ) {
+    if(a > c)  {
+        Console.Write($"max {a}");
+    } else {
+        Console.Write($"max {c}");
+    }
 } else {
-    Console.Write($"max {b}");
+    if(b > c) {
+        Console.Write($"max {b}");
+    } else {
+        Console.Write($"max {c}");
+    }
 }
