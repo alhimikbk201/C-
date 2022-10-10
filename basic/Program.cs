4cbe1373275e69436382e2a616d9ecf5abf4fbdb
Alexandra788 <andeytrof45@gmail.com>
2022-10-10 23:35:43 +0700

    Сделана и протестирована задача 2, остальные задачи будут в ветках

--- basic/Program.cs
+++ basic/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a, b;
+
+string number1, number2;
+Console.Write("Введите первое число");
+
+number1 = Console.ReadLine()!;
+
+int.TryParse(number1, out a);
+
+Console.Write("Введите первое число");
+
+number2 = Console.ReadLine()!;
+
+int.TryParse(number2, out b);
+
+if(a > b) {
+    Console.Write($"max {a}");
+} else {
+    Console.Write($"max {b}");
+}
