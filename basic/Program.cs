8ba87869d30781f65737069ee152d4862f5d5064
Alexandra788 <andeytrof45@gmail.com>
2022-10-11 00:38:57 +0700

    сделана и протестирована задача 8 первой лекции

--- basic/Program.cs
+++ basic/Program.cs
@@ -1,19 +1,24 @@
-﻿int a;
+﻿int n;
 
 string number1;
 Console.Write("Введите первое число");
 
 number1 = Console.ReadLine()!;
 
-int.TryParse(number1, out a);
+int.TryParse(number1, out n);
+
+int i = 1;
+
+while(i <= n) {
+    if (i % 2 == 0) {
+    Console.Write($"{i} ");
+} 
+
+i++;
+} 
 
 
 
 
 
 
-if (a % 2 == 0) {
-    Console.Write($"{a} четное");
-} else {
-    Console.Write($"{a} не четное");
-}
