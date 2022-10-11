fff092b38144fe4ebc6c99bbf9e8200dd9b225f8
Alexandra788 <andeytrof45@gmail.com>
2022-10-11 23:31:14 +0700

    Написана функция

--- basic/Program.cs
+++ basic/Program.cs
@@ -1,4 +1,17 @@
-﻿int a;
+﻿void DayOfTheWeek(int _a) {
+    if(_a == 6 || _a == 7) {
+        Console.Write("Выходной");
+} else {
+    if(_a > 7) {
+        Console.Write("Не допустимое значение, число должно быть от 1 до 7");
+    } else {
+        Console.Write("Будний день");
+    }
+}
+
+}
+
+int a;
 
 string number1;
 Console.Write("Введите число");
@@ -7,15 +20,7 @@ number1 = Console.ReadLine()!;
 
 int.TryParse(number1, out a);
 
+DayOfTheWeek(a);
 
 
-if(a == 6 || a == 7) {
-    Console.Write("Выходной");
-} else {
-    if(a > 7) {
-        Console.Write("Не допустимое значение, число должно быть от 1 до 7");
-    } else {
-        Console.Write("Будний день");
-    }
-}
    
