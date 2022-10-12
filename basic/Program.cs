ae5549a4c7aae4fc75d1486417cc57552486cc67
Alexandra788 <andeytrof45@gmail.com>
2022-10-12 18:00:54 +0700

    Сделана ипротестирована

--- basic/Program.cs
+++ basic/Program.cs
@@ -1,15 +1,14 @@
 ﻿void CubeNumber(int _a) {
-
-    
-
-    
-}
+    for(int i = 1; i <= _a; i++){
+    Console.Write($"{Math.Pow(i, 3)} ");
+    }
+    }
 int a;
 string number1;
-Console.Write("Введите первое число");
+Console.Write("Введите число");
 
 number1 = Console.ReadLine()!;
-
-a = 
+int.TryParse(number1, out a);
+ 
 
 CubeNumber(a);
