be4cb368938d2d4597f26393b5415d8a6206ffbc
Alexandra788 <andeytrof45@gmail.com>
2022-10-12 22:42:59 +0700

    сделана и протестирована

--- basic/Program.cs
+++ basic/Program.cs
@@ -1,14 +1,28 @@
-﻿void CubeNumber(int _a) {
-    for(int i = 1; i <= _a; i++){
-    Console.Write($"{Math.Pow(i, 3)} ");
-    }
-    }
-int a;
-string number1;
-Console.Write("Введите число");
+﻿void distanceDot (int [] _A, int [] _B) {
+    double _s;
 
-number1 = Console.ReadLine()!;
-int.TryParse(number1, out a);
- 
+    _s = Math.Sqrt(Math.Pow((_A[0] - _B[0]), 2) + Math.Pow((_A[1] - _B[1]), 2) + Math.Pow((_A[2] - _B[2]), 2));
+    Console.Write(_s);
+}
 
-CubeNumber(a);
+int [] A = {};
+int [] B = {};
+
+
+int [] inputDot() {
+int [] _A = {0, 0 , 0}; 
+Console.Write("Введите X");
+int.TryParse(Console.ReadLine()!, out _A[0]);
+Console.Write("Введите Y");
+int.TryParse(Console.ReadLine()!, out _A[1]); 
+Console.Write("Введите Z");
+int.TryParse(Console.ReadLine()!, out _A[2]); 
+
+return _A;
+}
+
+A = inputDot();
+B = inputDot();
+
+distanceDot(A, B);
+
