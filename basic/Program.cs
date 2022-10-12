60c355eb628e4e38e86526fe897318c92495f5ff
Alexandra788 <andeytrof45@gmail.com>
2022-10-13 01:45:41 +0700

    Сделана и протестирована

--- basic/Program.cs
+++ basic/Program.cs
@@ -7,4 +7,32 @@ number1 = Console.ReadLine()!;
 
 int.TryParse(number1, out a);
 
-Console.Write(a % 10);
+int i = 0;
+int j = 4;
+int c, v;
+
+a += 100000;
+while(i <= 2 ) {
+  
+    c = a / (int)Math.Pow(10, j) % 10;
+    v = a / (int)Math.Pow(10, i) % 10;
+    if(c != v ) {
+        Console.Write("Не полиндром  ");
+       
+       
+        break;
+
+    } else {
+        
+        i++;
+        j--;
+
+        if(i == 2) {
+            Console.Write("Полиндром");
+}
+    }
+
+   
+
+}
+
