d0a2943e7d0b1fb42c9ef9664b7957229b352c26
Alexandra788 <andeytrof45@gmail.com>
2022-10-18 00:12:33 +0700

    сделана и протестированна программа

--- basic/Program.cs
+++ basic/Program.cs
@@ -1,10 +1,48 @@
 ﻿int a;
 
 string number1;
-Console.Write("Введите число");
+Console.Write("Введите длинну массива");
 
 number1 = Console.ReadLine()!;
 
 int.TryParse(number1, out a);
 
-Console.Write(a % 10);
+int [] arr = {};
+
+arr = getArr(a);
+ 
+ 
+printArr(arr);
+
+howEvenNubersInArr(arr);
+
+void howEvenNubersInArr (int [] _arr) {
+   int count = 0;
+
+   for(int i = 0; i < _arr.Length; i++) {
+    if(_arr[i] % 2 == 0) {
+        count ++;
+
+    } 
+   }
+
+   Console.Write($"Количество четных чисел в массиве равно {count}");
+}
+
+
+int [] getArr(int _a) {
+    
+    int [] _arr = new int [_a];
+    
+    for(int i = 0; i < _a; i++) {
+        _arr[i] = new Random().Next(100, 1000);
+        }
+
+        return _arr;
+} 
+
+void printArr(int[] _arr) {
+    for(int i = 0; i < _arr.Length; i++) {
+        Console.Write($"{_arr[i]} ");
+    }
+} 
