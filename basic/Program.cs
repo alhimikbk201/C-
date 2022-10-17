7b6d66af921d9a44166c665b2abb7af0921ee032
Alexandra788 <andeytrof45@gmail.com>
2022-10-18 01:39:48 +0700

    сделана и протестирована функция нахождения максимального числа в массиве

--- basic/Program.cs
+++ basic/Program.cs
@@ -7,4 +7,47 @@ number1 = Console.ReadLine()!;
 
 int.TryParse(number1, out a);
 
-Console.Write(a % 10);
+int [] arr = {};
+
+arr = getArr(a);
+
+printArr(arr);
+
+int resolt = getMaxMeaningFromArr(arr);
+
+Console.Write($"Max number {resolt}");
+
+
+int getMaxMeaningFromArr(int[] _arr) {
+   int meaning = _arr[0];
+
+   for(int i = 1; i < _arr.Length; i++) {
+    if(meaning < _arr[i]) {
+
+        meaning = _arr[i];
+        }
+
+    }
+
+   return meaning;
+}
+
+
+
+int [] getArr(int _a) {
+    
+    int [] _arr = new int [_a];
+    
+    for(int i = 0; i < _a; i++) {
+        _arr[i] = new Random().Next(1, 100);
+        }
+
+        return _arr;
+} 
+
+void printArr(int[] _arr) {
+    for(int i = 0; i < _arr.Length; i++) {
+        Console.Write($"{_arr[i]} ");
+    }
+} 
+
