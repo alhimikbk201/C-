097bdc0f26d6e464ac91a809349347de84893a67
Alexandra788 <andeytrof45@gmail.com>
2022-10-18 01:10:05 +0700

    Нужно добавить функцию вывода массива

--- basic/Program.cs
+++ basic/Program.cs
@@ -7,4 +7,34 @@ number1 = Console.ReadLine()!;
 
 int.TryParse(number1, out a);
 
-Console.Write(a % 10);
+int [] arr = {};
+
+arr = getArr(a);
+
+
+
+int [] getArr(int _a) {
+    
+    int [] _arr = new int [_a];
+    
+    for(int i = 0; i < _a; i++) {
+        _arr[i] = new Random().Next(1, 100);
+        }
+
+        return _arr;
+} 
+
+void getSumNotevenPositionArr(int []_arr) {
+
+    int sum = 0;
+
+    for(int i = 0; i < _arr.Length; i++) {
+        if(i % 2 != 0) {
+            sum += _arr[i];
+        }
+
+        Console.Write($"Сумма значений нечетный позиций равна {sum}");
+        
+    }
+
+}
