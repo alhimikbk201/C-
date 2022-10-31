de147f1792fa6d4035ebe5f22e60f10014a018e3
Alexandra788 <andeytrof45@gmail.com>
2022-11-01 04:40:24 +0700

    сделана и протестирована программа

--- basic/Program.cs
+++ basic/Program.cs
@@ -1,10 +1,37 @@
-﻿int a;
+﻿double k1 = 0, b1 = 0, k2 = 0, b2 = 0;
+double x, y;
 
-string number1;
-Console.Write("Введите число");
+string number1 = "", number2 = "", number3 = "", number4 = "";
+
+Console.Write("Введите " + nameof(k1));
 
 number1 = Console.ReadLine()!;
 
-int.TryParse(number1, out a);
+double.TryParse(number1, out k1);
 
-Console.Write(a % 10);
+Console.Write("Введите " + nameof(b1));
+
+number2 = Console.ReadLine()!;
+
+double.TryParse(number2, out b1);
+
+Console.Write("Введите " + nameof(k2));
+
+number3 = Console.ReadLine()!;
+
+double.TryParse(number3, out k2);
+
+Console.Write("Введите " + nameof(b2));
+
+number4 = Console.ReadLine()!;
+
+double.TryParse(number4, out b2);
+
+
+x = (b2 - b1)/(k1 - k2);
+y = k1*x + b1;
+
+Console.Write($"Точка пересечения ({x}, {y})");
+
+
+
