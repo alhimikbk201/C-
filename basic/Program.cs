7a2ba10efb43654401c1763d35f5941a5ce3d029
Alexandra788 <andeytrof45@gmail.com>
2022-11-04 21:38:36 +0700

    сделана и протестирована программа

--- basic/Program.cs
+++ basic/Program.cs
@@ -1,10 +1,30 @@
-﻿int a;
+﻿int n,m;
 
-string number1;
-Console.Write("Введите число");
+string number1, number2;
+
+Console.Write("Введите число строк");
 
 number1 = Console.ReadLine()!;
 
-int.TryParse(number1, out a);
+int.TryParse(number1, out m);
 
-Console.Write(a % 10);
+
+Console.Write("Введите число столбцов");
+
+number2 = Console.ReadLine()!;
+
+int.TryParse(number1, out n);
+
+double[,] array = new double [m, n];
+
+for(int i = 0; i < m; i++) {
+    for(int j = 0; j < n; j++) {
+
+        array[i, j] = Math.Round(new Random().NextDouble()*20 - 10, 1);
+        Console.Write($"{array[i, j]} ");
+
+    }
+
+    Console.WriteLine();
+} 
+
