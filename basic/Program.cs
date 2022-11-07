26ab22edddad77138238140907e2e3618a85fa0b
Alexandra788 <andeytrof45@gmail.com>
2022-11-07 17:34:56 +0700

    сделана и протестирована программа

--- basic/Program.cs
+++ basic/Program.cs
@@ -1,29 +1,42 @@
-﻿int n, m;
+﻿int n, m, A;
 
 string number1, number2;
 
-Console.Write("Введите число наименьшее число диапозона ");
+Console.Write("Введите число  ");
 
 number1 = Console.ReadLine()!;
 
 int.TryParse(number1, out m);
 
 
-Console.Write("Введите наибольшее число диапозона ");
+Console.Write("Введите число ");
 
 number2 = Console.ReadLine()!;
 
 int.TryParse(number2, out n);
 
-PrintNaturalNumber(m, n);
 
 
-void PrintNaturalNumber(int _lesserNumber, int _moreNumber) {
 
-    if(_moreNumber >= _lesserNumber ) {
-        Console.Write($"{_lesserNumber} ");
-        PrintNaturalNumber(_lesserNumber + 1, _moreNumber);
-    } 
+A = ack(m, n);
 
+Console.Write($"{A}");
+
+int ack(int _m, int _n) {
+
+
+    if (_m == 0) {
+        return _n + 1;
+    }
+    else if ((_m > 0) && (_n == 0)) {
+        return ack(_m - 1, 1);
+    }
+    else if ((_m > 0) && (_n > 0)) {
+        return ack(_m - 1, ack(_m, _n - 1));
+    } else {
+        return 0;
+    }
+
+    
 }
 
