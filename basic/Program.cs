bbf8c04c1afbd98a97cdb2d394dc5579bfaf211e
Alexandra788 <andeytrof45@gmail.com>
2022-11-07 15:06:21 +0700

    сделана и протестирована программа

--- basic/Program.cs
+++ basic/Program.cs
@@ -1,10 +1,29 @@
-﻿int a;
+﻿int n, m;
 
-string number1;
-Console.Write("Введите число");
+string number1, number2;
+
+Console.Write("Введите число наименьшее число диапозона ");
 
 number1 = Console.ReadLine()!;
 
-int.TryParse(number1, out a);
+int.TryParse(number1, out m);
 
-Console.Write(a % 10);
+
+Console.Write("Введите наибольшее число диапозона ");
+
+number2 = Console.ReadLine()!;
+
+int.TryParse(number2, out n);
+
+PrintNaturalNumber(m, n);
+
+
+void PrintNaturalNumber(int _lesserNumber, int _moreNumber) {
+
+    if(_moreNumber >= _lesserNumber ) {
+        Console.Write($"{_lesserNumber} ");
+        PrintNaturalNumber(_lesserNumber + 1, _moreNumber);
+    } 
+
+}
+
