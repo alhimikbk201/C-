b4723fc982a2ab6de495906ac1139239fdd95bcf
Alexandra788 <andeytrof45@gmail.com>
2022-11-07 14:56:28 +0700

    сделана и протестирована программа

--- basic/Program.cs
+++ basic/Program.cs
@@ -1,30 +1,23 @@
-﻿int n,m;
+﻿int n;
 
-string number1, number2;
-
-Console.Write("Введите число строк");
-
-number1 = Console.ReadLine()!;
-
-int.TryParse(number1, out m);
+string number2;
 
 
-Console.Write("Введите число столбцов");
+Console.Write("Введите число ");
 
 number2 = Console.ReadLine()!;
 
-int.TryParse(number1, out n);
+int.TryParse(number2, out n);
 
-double[,] array = new double [m, n];
+PrintNaturalNumber(n);
 
-for(int i = 0; i < m; i++) {
-    for(int j = 0; j < n; j++) {
 
-        array[i, j] = Math.Round(new Random().NextDouble()*20 - 10, 1);
-        Console.Write($"{array[i, j]} ");
+void PrintNaturalNumber(int _number) {
 
-    }
+    if(_number >= 1) {
+        Console.Write($"{_number} ");
+        PrintNaturalNumber(_number - 1);
+    } 
 
-    Console.WriteLine();
-} 
+}
 
